b72b213bf718ae902b08736d29863862a1fc73e2
ECG Robotics <team1533@gmail.com>
2016-03-17 09:09:03 -0400

    Configured git on new drivers station

---
+++ src/org/usfirst/frc/team1533/scorpius/Panzer.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team1533.scorpius;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+
+public class Panzer {
+	
+	//State vars
+	static RobotDrive drive;
+	 static boolean fixedHeading; //MOVE //Implement in Sensory
+	 
+	//Smoothing vars
+	static double yTarg;
+	static double xTarg;
+		
+	public static void Initialize () {
+		drive = new RobotDrive(ConstantFactory.RobotMap.L_TREAD, ConstantFactory.RobotMap.R_TREAD);
+	}
+	public static void Drive(double xTarg, double yTarg){
+		drive.arcadeDrive(yTarg, xTarg, true);
+	}
+	
+	public static void Update(){ //DEPLOY
+		yTarg = Extensions.Lerp(yTarg, -Sensory.GetAxis(1, Sensory.tankOverride() ? 0 : 1)*(Swerve.flipMotors ? -1 : 1), ConstantFactory.Panzer.HARDNESS_CONSTANT * 0.033);
+		xTarg = Extensions.Lerp(xTarg, -Sensory.GetAxis(2, Sensory.tankOverride() ? 0 : 1)*(Swerve.flipMotors ? -1 : 1), ConstantFactory.Panzer.HARDNESS_CONSTANT * 0.033); //not kTwist
+		Drive(yTarg, fixedHeading ? Gyro.GetAngle() * ConstantFactory.Panzer.HEADING_SCALE_FACTOR * -1 : xTarg);
+	}
+	public static void lockWheels(){
+		yTarg = 0;
+		xTarg = 0;
+	}
+}
---
+++ src/org/usfirst/frc/team1533/scorpius/Stinger.java
@@ -0,0 +1,82 @@
+package org.usfirst.frc.team1533.scorpius;
+
+import java.util.Timer;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class Stinger {
+	
+	//State vars
+	static SpeedController stingerL;
+	static SpeedController stingerR;
+	static SpeedController roller;
+	static double angle;
+	//Timing vars
+	static Timer timer;
+	static int buttonPressed;
+	static long shootStartTime = -1;
+	
+	public static void Initialize () {
+		//Initialize Sparks
+		stingerL = new Spark(ConstantFactory.RobotMap.STINGER_L);
+		stingerR = new Spark(ConstantFactory.RobotMap.STINGER_R);
+		roller = new Spark(ConstantFactory.RobotMap.ROLLER);
+		//Initialize Timer
+		timer = new Timer();
+	}
+	public static void runStingerMotor(){
+		if(buttonPressed == 0){
+			//shoots ball
+			stingerL.set(-ConstantFactory.Stinger.STINGER_POWER_SHOOT_PERCENT);
+			stingerR.set(ConstantFactory.Stinger.STINGER_POWER_SHOOT_PERCENT);
+		}
+		else if(buttonPressed == 1){
+			//grabs ball
+			stingerL.set(ConstantFactory.Stinger.STINGER_POWER_GRASP_PERCENT);
+			stingerR.set(-ConstantFactory.Stinger.STINGER_POWER_GRASP_PERCENT);
+
+		}
+		else if(buttonPressed == 2){
+			stingerL.set(0);
+			stingerR.set(0);
+		}
+	}
+	public static void runRollerMotor(int buttonPressed){
+		if (buttonPressed == 0) {
+			//shoots ball
+			roller.set(ConstantFactory.Stinger.STINGER_POWER_SHOOT_PERCENT);
+		}
+		else if(buttonPressed == 1) {
+			//grabs ball
+			roller.set(-ConstantFactory.Stinger.STINGER_POWER_SHOOT_PERCENT);
+		}
+		else if(buttonPressed == 2){
+			roller.set(0);
+		}
+	}
+	public static void Update () {
+		//Hold down to shoot
+		if (Sensory.GetButtonDown(ButtonMapping.RIGHT_BUMPER, 1)) {
+			runStingerMotor();
+			if (shootStartTime < 0) {
+				shootStartTime = System.currentTimeMillis();
+			} else if (System.currentTimeMillis()-shootStartTime > ConstantFactory.Stinger.SHOOTER_DELAY * 1000) {
+				runRollerMotor(0);
+			}
+		}
+		//Hold down to grab ball
+		else if (Sensory.GetButtonDown(ButtonMapping.RIGHT_TRIGGER, 1)) {
+			runRollerMotor(1);
+			runStingerMotor();
+			shootStartTime = -1;
+		}
+		//Slow all motors
+		else {
+			runRollerMotor(2);
+			runStingerMotor();
+			shootStartTime = -1;
+		}
+
+	}
+}
